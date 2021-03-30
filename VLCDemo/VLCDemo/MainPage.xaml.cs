1208967813ed8479b86ad6f19b558c87f34c57ed
Damien Tohin Doumer Kake <damientohin@gmail.com>
2021-03-30 23:21:12 +0200

    Initial setup with video view on android

--- VLCDemo/VLCDemo/MainPage.xaml.cs
+++ VLCDemo/VLCDemo/MainPage.xaml.cs
@@ -4,15 +4,37 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LibVLCSharp.Shared;
 using Xamarin.Forms;
 
 namespace VLCDemo
 {
     public partial class MainPage : ContentPage
     {
+        LibVLC _libVLC;
+        MediaPlayer _mediaPlayer;
+
         public MainPage()
         {
             InitializeComponent();
         }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            //VideoView.MediaPlayerChanged += MediaPlayerChanged;
+
+            Core.Initialize();
+
+            _libVLC = new LibVLC();
+            _mediaPlayer = new MediaPlayer(_libVLC)
+            {
+                Media = new Media(_libVLC, new Uri("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"))
+            };
+
+            VideoView.MediaPlayer = _mediaPlayer;
+            _mediaPlayer.Play();
+        }
+
     }
 }
